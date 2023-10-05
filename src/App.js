69ef042814f462704c2cd03ec58ea762a8244cda
mokaddemrachid <rachidmokaddem068@gmail.com>
2023-10-05 10:23:38 +0100

    map

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d102249.31752462024!2d10.151169360944213!3d36.787568604837055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd337f5e7ef543%3A0xd671924e714a0275!2sTunis!5e0!3m2!1sfr!2stn!4v1696494281422!5m2!1sfr!2stn"
+              width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"/>
+    </div>
+  );
+}
+
+export default App;
